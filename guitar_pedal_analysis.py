8face8106d1f304eb6ad5e0ddd944628e849c02c
J. Gier <joshuasgier@gmail.com>
2022-04-02 09:44:26 -0700

    adding python script for analysis

---
+++ guitar_pedal_analysis.py
@@ -0,0 +1,10 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+#recording some input values here
+input_vpp = 300E-3 #max expected vpp of guitar input, no amplification, 0V offset. Estimate only
+
+
+
+
