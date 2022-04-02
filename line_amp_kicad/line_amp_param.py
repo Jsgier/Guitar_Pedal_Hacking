456b83e6a519baa64f6353bcd3cacac091f94a65
Josh Gier <josh@soeffects.com>
2022-04-02 15:06:19 -0700

    added a few lines to python program

--- line_amp_kicad/line_amp_param.py
+++ line_amp_kicad/line_amp_param.py
@@ -3,12 +3,27 @@ from matplotlib import pyplot as plt
 
 
 #organize parameters as a dictionary
+#these are default values to be replaced with calculation
 line_amp_params = {
         'rin': 10E6, #input impedance of guitar pickup
-        'c_dcb': 10E-9, #DC blocking capacitor
+        'c_dcb': 10E-9, #DC blocking capacitor, 
         'r1b': 10E3, #r1 in divider bias network
         'r2b': 10E3 #r2 in divider bias network
         }
+
+#--Calculations for determining component values--
+
+#- Constants
+fc_hpf = 100 #dc blocking capacitor filter cutoff frequency [Hz]
+
+#- Functions
+
+
+#- Calculations
+
+
+line_amp_params['c_dcb'] = 
+
 
 #open file and write parameters:
 with open('line_amp.txt', 'w') as f:
