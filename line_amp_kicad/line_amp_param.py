41a6967924d1ed75c6dfc80deac03370c1fa3461
Josh Gier <josh@soeffects.com>
2022-04-08 08:23:29 -0700

    Added readme w/ descriptions and next steps

--- line_amp_kicad/line_amp_param.py
+++ line_amp_kicad/line_amp_param.py
@@ -10,7 +10,9 @@ params = {
         'r1b': 10E3, #r1 in divider bias network
         'r2b': 10E3, #r2 in divider bias network
         'rf' : 1E3, #rf in non inverting amplifier
-        'rg' : 1E3 #rg in non inverting amplifier
+        'rg' : 1E3, #rg in non inverting amplifier
+        'raa' : 50, #antialiasing filter resistor
+        'caa' : 1E-9 #antialiasing filter capacitor
         }
 
 #--Calculations for determining component values--
@@ -28,11 +30,13 @@ def wc(r1,r2,cdcb): return 1 / (2*np.pi * R_parallel(r1,r2) * cdcb)
 def calc_dcb(r1,r2,fc): return 1 / (2*np.pi * R_parallel(r1,r2) * fc)
 def calc_gain(rf,rg): return 1 + rf / rg
 def calc_rg(gain, rf): return rf / (gain - 1) 
+def calc_c_lpf(fc, r): return 1 / (2*np.pi*fc*r)
 
 #- Calculations
-params['r2b'] = calc_r2(5, 1.65, params['r1b']) 
-params['c_dcb'] = calc_dcb(params['r1b'], params['r2b'], fc_hpf)
-params['rg'] = calc_rg(gain = vout / vin, rf = params['rf']) 
+params['r2b'] = calc_r2(vs = 5, vb = 1.65, params['r1b']) #calculate r2 for bias network
+params['c_dcb'] = calc_dcb(params['r1b'], params['r2b'], fc_hpf) #calculate dc blocking cap for fc
+params['rg'] = calc_rg(gain = vout / vin, rf = params['rf']) #calculate gain resistor for amp
+params['caa'] = calc_c_lpf(fc = 50E3, params['raa']) #calculate antialiasing filter capacitor value
 
 
 #open file and write parameters
